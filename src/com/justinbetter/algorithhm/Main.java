229100818a2231a8f5b3b6f5fe6f3a6b9ba5d955
hejia <hejia01@qiyi.com>
2020-08-06 10:27:46 +0800

    first commit

---
+++ src/com/justinbetter/algorithhm/Main.java
@@ -0,0 +1,8 @@
+package com.justinbetter.algorithhm;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/justinbetter/algorithhm/code1.java
@@ -0,0 +1,9 @@
+package com.justinbetter.algorithhm;
+
+public class code1 {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("test a code1");
+    }
+}
