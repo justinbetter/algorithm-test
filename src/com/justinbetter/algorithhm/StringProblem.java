9715b496e62cc114f2c09832bb476d51ee434c8d
justin <justinbetter03@gmail.com>
2020-09-13 18:23:46 +0800

    feature: SolutionLongestCommonPrefix

--- src/com/justinbetter/algorithhm/StringProblem.java
+++ src/com/justinbetter/algorithhm/StringProblem.java
@@ -26,4 +26,37 @@ public class StringProblem {
         }
         return res;
     }
+
+    //最长公共前缀
+   static class SolutionLongestCommonPrefix {
+        public String longestCommonPrefix(String[] strs) {
+            if (strs == null || strs.length == 0) {
+                return "";
+            } else {
+                return longestCommonPrefix(strs, 0, strs.length - 1);
+            }
+        }
+
+        public String longestCommonPrefix(String[] strs, int start, int end) {
+            if (start == end) {
+                return strs[start];
+            } else {
+                int mid = (end - start) / 2 + start;
+                String lcpLeft = longestCommonPrefix(strs, start, mid);
+                String lcpRight = longestCommonPrefix(strs, mid + 1, end);
+                return commonPrefix(lcpLeft, lcpRight);
+            }
+        }
+
+        public String commonPrefix(String lcpLeft, String lcpRight) {
+            int minLength = Math.min(lcpLeft.length(), lcpRight.length());
+            for (int i = 0; i < minLength; i++) {
+                if (lcpLeft.charAt(i) != lcpRight.charAt(i)) {
+                    return lcpLeft.substring(0, i);
+                }
+            }
+            return lcpLeft.substring(0, minLength);
+        }
+    }
+
 }
