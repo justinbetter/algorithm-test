17539d2280e1736de9e3272942ad1791f5a42134
hejia <hejia01@qiyi.com>
2020-09-14 15:12:21 +0800

    feat: 两数相乘

--- src/com/justinbetter/algorithhm/StringProblem.java
+++ src/com/justinbetter/algorithhm/StringProblem.java
@@ -9,6 +9,61 @@ public class StringProblem {
         System.out.println(lengthOfLongestSubstring("aaabcssds"));
     }
 
+    class SolutionMultiply {
+
+        public String multiply(String num1, String num2) {
+            if (num1.equals("0") || num2.equals("0")) {
+                return "0";
+            }
+            String ans = "0";
+            int m = num1.length(), n = num2.length();
+            //除数倒序相乘
+            for (int i = n - 1; i >= 0; i--) {
+                StringBuffer curr = new StringBuffer();
+                int add = 0;
+                //补0
+                for (int j = n - 1; j > i; j--) {
+                    curr.append(0);
+                }
+                //获取被除数
+                int y = num2.charAt(i) - '0';
+                //依次相乘
+                for (int j = m - 1; j >= 0; j--) {
+                    //获取被除数
+                    int x = num1.charAt(j) - '0';
+                    int product = x * y + add;
+                    //添加余数
+                    curr.append(product % 10);
+                    //添加整数作为进位
+                    add = product / 10;
+                }
+                if (add != 0) {
+                    curr.append(add % 10);
+                }
+                ans = addStrings(ans, curr.reverse().toString());
+            }
+            return ans;
+        }
+        //字符串相加
+        public String addStrings(String num1, String num2) {
+            int i = num1.length() - 1, j = num2.length() - 1, add = 0;
+            StringBuffer ans = new StringBuffer();
+            while (i >= 0 || j >= 0 || add != 0) {
+                int x = i >= 0 ? num1.charAt(i) - '0' : 0;
+                int y = j >= 0 ? num2.charAt(j) - '0' : 0;
+                int result = x + y + add;
+                ans.append(result % 10);
+                add = result / 10;
+                i--;
+                j--;
+            }
+            ans.reverse();
+            return ans.toString();
+        }
+    }
+
+
+    //获取最长不重复子序列
     static int lengthOfLongestSubstring(String s) {
         //思路：滑动窗口
         // i j 滑动判断字符重复,重复则跳到该边界之外，因为在这个字符内不可能出现更小的子序列了
@@ -34,7 +89,7 @@ public class StringProblem {
             //保存每个字母出现的次数
             int length1 = s1.length();
             int length2 = s2.length();
-            if (length1 > length2){
+            if (length1 > length2) {
                 return false;
             }
             int[] c1 = new int[26];
@@ -50,7 +105,7 @@ public class StringProblem {
                 if (mathches(c1, c2)) {
                     return true;
                 }
-                c2[s2.charAt(i-length1) - 'a']--;
+                c2[s2.charAt(i - length1) - 'a']--;
                 c2[s2.charAt(i) - 'a']++;
             }
             return mathches(c1, c2);
