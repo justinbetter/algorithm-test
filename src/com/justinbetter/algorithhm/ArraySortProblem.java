24b1ccbbc0f503b2b646bf5b56c132559d9ccbc3
justin <justinbetter03@gmail.com>
2020-09-16 23:26:48 +0800

    feature: trap

--- src/com/justinbetter/algorithhm/ArraySortProblem.java
+++ src/com/justinbetter/algorithhm/ArraySortProblem.java
@@ -9,6 +9,63 @@ public class ArraySortProblem {
         int[] nums = new int[]{1, 32, 1, 2, 4, 5, 6, 0, 19};
         System.out.println(new findKthLargestSolution().findKthLargest2(nums, 2));
     }
+    // 接雨水
+    //最两端的列不用考虑，因为一定不会有水。所以下标从 1 到 length - 2
+//    注意雨水单位计算方式是该列左右两边最高的墙中 选择一个较矮的高度-当前列高度就是该列积累的雨水
+//    该列大于等于较矮高度，不积水
+//    该列小于较矮高度，积水=较矮高度-当前列高度
+    class trapSolution {
+
+        public int trap1(int[] height) {
+            int sum = 0;
+            //最两端的列不用考虑，因为一定不会有水。所以下标从 1 到 length - 2
+            for (int i = 1; i < height.length - 1; i++) {
+                int max_left = 0;
+                //找出左边最高
+                for (int j = i - 1; j >= 0; j--) {
+                    if (height[j] > max_left) {
+                        max_left = height[j];
+                    }
+                }
+                int max_right = 0;
+                //找出右边最高
+                for (int j = i + 1; j < height.length; j++) {
+                    if (height[j] > max_right) {
+                        max_right = height[j];
+                    }
+                }
+                //找出两端较小的
+                int min = Math.min(max_left, max_right);
+                //只有较小的一段大于当前列的高度才会有水，其他情况不会有水
+                if (min > height[i]) {
+                    sum = sum + (min - height[i]);
+                }
+            }
+            return sum;
+        }
+
+        //动态规划法：左边最高的就是上一个最高的和当前左边第一个比较；右边同理；
+        //循环列，计算雨水总和
+        public int trap2(int[] height) {
+            int sum = 0;
+            int[] max_left = new int[height.length];
+            int[] max_right = new int[height.length];
+
+            for (int i = 1; i < height.length - 1; i++) {
+                max_left[i] = Math.max(max_left[i - 1], height[i - 1]);
+            }
+            for (int i = height.length - 2; i >= 0; i--) {
+                max_right[i] = Math.max(max_right[i + 1], height[i + 1]);
+            }
+            for (int i = 1; i < height.length - 1; i++) {
+                int min = Math.min(max_left[i], max_right[i]);
+                if (min > height[i]) {
+                    sum = sum + (min - height[i]);
+                }
+            }
+            return sum;
+        }
+    }
 
     //第k个排列
     //深度遍历+剪枝
