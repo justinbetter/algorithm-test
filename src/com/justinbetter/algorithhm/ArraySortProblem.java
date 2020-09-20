9da6df34c56bb5793cf63b25c9695efd70b13177
hejia <hejia01@qiyi.com>
2020-09-20 23:17:02 +0800

    feat: longestIncreasingPathSolution

--- src/com/justinbetter/algorithhm/ArraySortProblem.java
+++ src/com/justinbetter/algorithhm/ArraySortProblem.java
@@ -10,10 +10,44 @@ public class ArraySortProblem {
         System.out.println(new findKthLargestSolution().findKthLargest2(nums, 2));
     }
 
-    //TODO 矩阵中的最长递增路径
+    //矩阵中的最长递增路径
     class longestIncreasingPathSolution {
+        public int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        public int rows, columns;
+
         public int longestIncreasingPath(int[][] matrix) {
-            return -1;
+            if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+                return 0;
+            }
+            rows = matrix.length;
+            columns = matrix[0].length;
+            int[][] memo = new int[rows][columns];
+            int ans = 0;
+            for (int i = 0; i < rows; ++i) {
+                for (int j = 0; j < columns; ++j) {
+                    ans = Math.max(ans, dfs(matrix, i, j, memo));
+                }
+            }
+            return ans;
+        }
+
+        public int dfs(int[][] matrix, int row, int column, int[][] memo) {
+            //表示记录过了
+            if (memo[row][column] != 0) {
+                return memo[row][column];
+            }
+            //记录下该位置
+            ++memo[row][column];
+            //开始移动
+            for (int[] dir : dirs) {
+                //行列移动
+                int newRow = row + dir[0], newColumn = column + dir[1];
+                //记录最大递增序列
+                if (newRow >= 0 && newRow < rows && newColumn >= 0 && newColumn < columns && matrix[newRow][newColumn] > matrix[row][column]) {
+                    memo[row][column] = Math.max(memo[row][column], dfs(matrix, newRow, newColumn, memo) + 1);
+                }
+            }
+            return memo[row][column];
         }
     }
 
