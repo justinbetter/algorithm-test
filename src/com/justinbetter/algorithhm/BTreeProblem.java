094c40045fd0a1a524c6bd8dead118899fadace9
hejia <hejia01@qiyi.com>
2020-09-20 21:42:45 +0800

    feat: 二叉树

---
+++ src/com/justinbetter/algorithhm/BTreeProblem.java
@@ -0,0 +1,39 @@
+package com.justinbetter.algorithhm;
+
+public class BTreeProblem {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+
+    //二叉树后序遍历 从下往上
+    //递归判断
+    TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // base case
+        if (root == null) return null;
+        if (root == p || root == q) return root;
+
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        // 情况 1
+        if (left != null && right != null) {
+            return root;
+        }
+        // 情况 2
+        if (left == null && right == null) {
+            return null;
+        }
+        // 情况 3
+        return left == null ? right : left;
+    }
+}
