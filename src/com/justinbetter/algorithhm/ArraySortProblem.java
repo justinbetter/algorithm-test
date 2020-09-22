8c1b7ca5fef2b4d28b7daa8b0a4b362a84e4f5c4
hejia <hejia01@qiyi.com>
2020-09-22 16:26:04 +0800

    feat: quickSort

--- src/com/justinbetter/algorithhm/ArraySortProblem.java
+++ src/com/justinbetter/algorithhm/ArraySortProblem.java
@@ -589,4 +589,90 @@ public class ArraySortProblem {
         }
     }
 
+    public class sortArraySolution {
+
+        // 快速排序 1：基本快速排序
+
+        /**
+         * 列表大小等于或小于该大小，将优先于 quickSort 使用插入排序
+         */
+        private final int INSERTION_SORT_THRESHOLD = 7;
+
+        private final Random RANDOM = new Random();
+
+
+        public int[] sortArray(int[] nums) {
+            int len = nums.length;
+            quickSort(nums, 0, len - 1);
+            return nums;
+        }
+
+        private void quickSort(int[] nums, int left, int right) {
+            // 小区间使用插入排序
+            if (right - left <= INSERTION_SORT_THRESHOLD) {
+                insertionSort(nums, left, right);
+                return;
+            }
+            //获取基准index，继续递归
+            int pIndex = partition(nums, left, right);
+            quickSort(nums, left, pIndex - 1);
+            quickSort(nums, pIndex + 1, right);
+        }
+
+        /**
+         * 对数组 nums 的子区间 [left, right] 使用插入排序
+         *
+         * @param nums  给定数组
+         * @param left  左边界，能取到
+         * @param right 右边界，能取到
+         */
+        private void insertionSort(int[] nums, int left, int right) {
+            for (int i = left + 1; i <= right; i++) {
+                //记录要插入的数据
+                int temp = nums[i];
+                int j = i;
+                while (j > left && nums[j - 1] > temp) {
+                    //当前值等于前一个值
+                    nums[j] = nums[j - 1];
+                    //设置当前值index为前一个
+                    j--;
+                }
+                //当前值等于temp，如果有插入，已经将需要插入的数组更新到后一个值了
+                nums[j] = temp;
+            }
+        }
+
+        private int partition(int[] nums, int left, int right) {
+            //快速排序
+            int randomIndex = RANDOM.nextInt(right - left + 1) + left;
+            swap(nums, left, randomIndex);
+
+            // 基准值
+            int pivot = nums[left];
+            int lt = left;
+            // 循环不变量：
+            // all in [left + 1, lt] < pivot
+            // all in [lt + 1, i) >= pivot
+            //循环数组
+            for (int i = left + 1; i <= right; i++) {
+                //小于基准，放左边
+                if (nums[i] < pivot) {
+                    //继续搜索
+                    lt++;
+                    //交换和左指针的位置，保证左边都是小于值
+                    swap(nums, i, lt);
+                }
+            }
+            //遍历结束后，交换左指针和基准的位置，返回左指针
+            swap(nums, left, lt);
+            return lt;
+        }
+
+        private void swap(int[] nums, int index1, int index2) {
+            int temp = nums[index1];
+            nums[index1] = nums[index2];
+            nums[index2] = temp;
+        }
+    }
+
 }
